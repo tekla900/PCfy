9f997f226115e28f83136cc3805c759ea120902a
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2023-01-17 19:17:01 +0400

    add select positions input

--- src/scripts/survey.js
+++ src/scripts/survey.js
@@ -1,6 +1,7 @@
-// addind options to team select
 const selectTeam = document.getElementById('team');
+const selectPosition = document.getElementById('position');
 
+// adding options to team and position select inputs
 fetch("https://pcfy.redberryinternship.ge/api/teams")
     .then(res => res.json())
     .then(res => {
@@ -12,3 +13,27 @@ fetch("https://pcfy.redberryinternship.ge/api/teams")
         });
     })
     .catch(error => console.log(error));
+
+// enabling position choosing after team is chosen
+selectTeam.addEventListener('change', () => {
+    fetch("https://pcfy.redberryinternship.ge/api/positions")
+    .then(response => response.json())
+    .then(res => {
+        // Filter out the positions that match the selected team
+        const positions = res.data.filter(position => position.team_id == selectTeam.value);
+        // Clear the options in the selectPosition element
+        selectPosition.innerHTML = "";
+
+        // Add the filtered positions as options in the selectPosition element
+        positions.forEach(position => {
+            const option = document.createElement("option");
+            option.value = position.id;
+            option.innerHTML = position.name;
+            selectPosition.appendChild(option);
+        });
+        selectPosition.disabled = false;
+    });
+});
+
+
+
